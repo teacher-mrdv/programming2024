0072b86dd27b243a7a5217b7484234e82f48fd8b
teacher Marcos <teacher.mrdv@gmail.com>
2024-10-21 13:47:27 +0800

    Create RecursionWorksheets1.java

---
+++ HL/Recursion/RecursionWorksheets1.java
@@ -0,0 +1,27 @@
+public class RecursionWorksheets1
+{
+    
+    public static int sum(int n)
+    {
+        if (n == 1)
+        {    return 1;
+        } else
+        {    return n + sum(n - 1);
+		}
+    }
+
+    public static int result(int n)
+    {
+        if (n == 1)
+        {    return 2;
+        } else
+        {    return 2 * result(n - 1);
+		}
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("sum(5) = " + sum(5));
+        System.out.println("result(5) = " + result(5));
+    }
+}
