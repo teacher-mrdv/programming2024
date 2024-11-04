6bf5818b3184505264218f79e3865428cbd61817
teacher Marcos <teacher.mrdv@gmail.com>
2024-11-04 10:02:27 +0800

    Update RecursionWorksheets1.java

--- HL/Recursion/RecursionWorksheets1.java
+++ HL/Recursion/RecursionWorksheets1.java
@@ -1,14 +1,14 @@
 public class RecursionWorksheets1
 {
-    }
+
     public static int sum(int n)
     {
-		System.out.print(n + "\t\" "); // out
-		System.out.println(n == 1);// output
+		System.out.print(n + " ");
+		System.out.println( (n == 1) );
         if (n == 1)
-        {    return 1;
+		{	return 1;
         } else
-        {    return n + sum(n - 1);
+        {	return n + sum(n - 1);
 		}
 		
     }
