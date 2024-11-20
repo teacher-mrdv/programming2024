21729a0ef02d0bce3eada2579195da5ea793ea9d
teacher Marcos <teacher.mrdv@gmail.com>
2024-11-20 17:44:34 +0800

    Create Snippets.java

---
+++ Demos/Snippets.java
@@ -0,0 +1,19 @@
+
+public class Snippets extends IBIO
+{
+	
+	public static void main (String[] args)
+	{
+		//int	a	=	17;
+		//int	b	=	a	%	10;
+		//if(	a	>	b	)
+		//{	output(	b	+	"	" +	a );
+		//}
+		//else
+		//{	output(	a	+	"	" +	b	);
+		//}
+
+		
+	}
+}
+
