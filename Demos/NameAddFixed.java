7337425ba08197b0da5132ce01ef57fb2964fd8e
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-02-13 17:45:14 +0800

    Create NameAddFixed.java

---
+++ Demos/NameAddFixed.java
@@ -0,0 +1,41 @@
+public class NameAddFixed
+{
+	static String[] BUGS = {"ANT", "FLY", "FREE", "", "", ""};
+	
+	static void add(String THISNAME)
+	{
+		int POS = 0;
+		while (!BUGS[POS].equals("FREE") && !BUGS[POS].equals(THISNAME))
+		{	POS = POS + 1;
+		}
+		if(POS < BUGS.length)
+		{
+			if(BUGS[POS] == "FREE")
+			{
+				BUGS[POS] = THISNAME;
+				if(POS < BUGS.length-1)
+				{	BUGS[POS + 1] = "FREE";
+				}
+			}
+		}
+	}
+	
+	static void printArray(String[] a)
+	{
+		for(int i = 0; i < a.length; i++)
+			System.out.printf("%d:%-9s", i, a[i]);
+		System.out.println();
+	}
+	
+	public static void main (String[] args)
+	{
+		printArray(BUGS);
+		add("BEE"); printArray(BUGS);
+		add("ANT"); printArray(BUGS);
+		add("FLY"); printArray(BUGS);
+		add("BEETLE"); printArray(BUGS);
+		add("CRICKET"); printArray(BUGS);
+		add("FIREFLY"); printArray(BUGS);
+	}
+}
+
