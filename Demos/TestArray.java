91e47cafce6b50bd8d938b4e9521f87e0491cbf9
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-02-26 12:07:42 +0800

    Update TestArray.java

--- Demos/TestArray.java
+++ Demos/TestArray.java
@@ -4,19 +4,31 @@ public class TestArray {
 
 	public static void printArray(String[] stringArray)
 	{
-		for(int i = 0; i < stringArray.length; i++)
-		{	System.out.print(stringArray[i] + " ");
+		System.out.print(stringArray[0]);
+		for(int i = 1; i < stringArray.length; i++)
+		{	System.out.print(", " + stringArray[i]);
 		}
+		System.out.println();
 	}
 
 	public static void printArray(int[] intArray)
 	{
-		for(int i = 0; i < intArray.length; i++)
-		{	System.out.print(intArray[i] + " ");
+		System.out.print(intArray[0]);
+		for(int i = 1; i < intArray.length; i++)
+		{	System.out.print(", " + intArray[i]);
+		}
+		System.out.println();
+	}
+
+	public static void initialiseStringArray(String[] s)
+	{
+		for(int i = 0; i < s.length; i++)
+		{	s[i] = "";
 		}
 	}
 	
-	public static void main (String[] args) {
+	public static void main (String[] args)
+	{
 		int[] a = new int[10];
 		int[] b = {1,2,3,4,5};
 		int[] c = {1,2,3,4,5};
@@ -31,6 +43,9 @@ public class TestArray {
 		System.out.println(a + " " + b + " " + c);
 		System.out.println(a == b);
 		System.out.println(a == c);
+		printArray(s);
+		System.out.println(s[1]);
+		initialiseStringArray(s);
 		printArray(s);
 		
 	}
