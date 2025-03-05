4d62561be6023822e8a2eaed8d49ad6b6f2d3037
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-03-05 20:21:49 +0800

    Update NestedLoops.java

--- Demos/NestedLoops.java
+++ Demos/NestedLoops.java
@@ -4,10 +4,18 @@ public class NestedLoops {
 	public static void main (String[] args) {
 		int c = 0;
 		for(int i = 0; i < 4; i++)
+			for(int j = 1; j < 4; j++)
+				for(int k = 1; k < 4; k++)
+				{	System.out.printf("%2d, %2d, %2d \n", i, j, k);
+					//System.out.println(i+ " " + j + " " + k);
+					c++;
+				}
+		System.out.println(c);
+		c = 0;
+		for(int i = 0; i < 4; i++)
 			for(int j = i; j < 4; j++)
 				for(int k = j; k < 4; k++)
 				{	System.out.printf("%2d, %2d, %2d \n", i, j, k);
-					//System.out.println(i+ " " + j + " " + k);
 					c++;
 				}
 		System.out.println(c);
