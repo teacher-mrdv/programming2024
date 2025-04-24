a0da4e0255ab541f6a3cf26cf59260c13bf87b53
Marcos Drien <teacher.mrdv@gmail.com>
2025-04-24 09:09:50 +0800

    Update LinkedListPlus.java

--- HL/ADT/LinkedListPlus.java
+++ HL/ADT/LinkedListPlus.java
@@ -1,146 +1,163 @@
-/*
- * Linked list
- */
-
 public class LinkedListPlus
 {
-	// https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
-	private static final String bold  = "\u001b[1m";
-	private static final String blink = "\u001b[5m";
-	private static final String invrs = "\u001b[7m";
-	private static final String reset = "\u001b[0m";
-	
-	Node start;
-	
-	public boolean isEmpty()
-	{
-		if(start == null)
-		{	return true;
-		} else
-		{	return false;
-		}
-		//return start == null;
-	}
+    Node start;
+    int size;
 
-	public void append(Node newNode)
-	{
-		if( isEmpty() )
-		{	start = newNode;
-			return;
-		}
-		Node temp = start;
-		while(temp.next != null)
-		{	temp = temp.next;
-		}
-		temp.next = newNode;
-	}
+    public LinkedList()
+    {
+        start = null;
+        size = 0;
+    } // creates an empty LL
 
-	public boolean remove(Node delete)
-	{	if( isEmpty() )
-		{	// System.out.println("Linked list is \[1mempty\[22m!");
-			return false;
-		}
-		if( delete.data == start.data )
-		{	start = start.next;
-			return true;
-		}
-		Node previous= start;
-		Node current = start.next;
-		//             vvv see methods in the vvv Node class
-		while( current.hasNext() && current.equals(delete))
-		// same as while( current.next != null && current.data != delete.data)
-		{	previous = previous.next;
-			current  = current.next;
-		}
-		if( current.data == delete.data)
-		{	previous.next = current.next;
-			return true;
-		}
-		return false;
-		/* OR...
-		if( current.data == delete.data)
-		{	previous.next = current.next;
-			return true;
-		} else
-		{	return false;
-		}
-		*/
-	}
+    public LinkedList(Node n) // creates a new LL with a starting node
+    {
+        start = n;
+        size = 1;
+    }
 
-	// insert BEFORE place
-	public void insert(Node insert, int place)
-	{
-		if(place-1 == 0)
-		{	insert.next = start;
-			start = insert;
-		}
-		int counter = 0;
-		Node temp = start;
-		while(counter < place)
-		{	temp = temp.next;
-			counter++;
-		}
-		insert = temp.next;
-		temp.next = insert;
-		
-	}
+    public boolean isEmpty()
+    {
+        return start == null;
+    }
 
-	// more typical IB question than adding a counter to the class
-	public int size()
-	{
-		Node temp = start;
-		int size = 0;
-		while( temp != null )
-		{	size++;
-			temp = temp.next;
-		}
-		return size;
-	}
+    public void append(Node n)
+    {
+        if (isEmpty())
+        {
+            start = n;
+            size = 1;
+            return;
+        }
+        Node temp = start;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = n;
+        size++;
+    }
 
-	public void printLL()	// NOT a built-in
-	{
-		Node temp = start;
-		System.out.print("Start -> ");
-		while( temp != null )
-		{	System.out.print( temp.data + " -> " );
-			temp = temp.next;
-		}
-		System.out.println("null");
-	}
+    public boolean delete(Node nodeToDelete)
+    {
+        if (isEmpty())
+        {
+            System.out.println("Error-linked list is empty");
+            return false;
+        }
+        if (start.data == nodeToDelete.data)
+        {
+            start = start.next;
+            size--;
+            return true;
+        }
+        Node prev = start;
+        Node current = start.next;
+        while (current != null)
+        {
+            if (current.data == nodeToDelete.data)
+            {
+                prev.next = current.next;
+                size--;
+                return true;
+            }
+            prev = prev.next;
+            current = current.next;
+        }
+        return false;
+    }
 
-	public static void main(String[] args)
-	{
-		LinkedListPlus ll = new LinkedListPlus();
-		System.out.println("Linked list is " +
-			invrs + bold + blink + " empty " + reset + "!");
-		System.out.println("\n>>> Appending 5, 3, 9, 7, 1, 6, 4 to the linked list (ll):");
-		Node n = new Node(5);
-		ll.append(n);	ll.printLL();
-		n = new Node(3);
-		ll.append(n);	ll.printLL();
-		n = new Node(7);
-		ll.append(n);	ll.printLL();
-		n = new Node(9);
-		ll.append(n);	ll.printLL();
-		n = new Node(1);
-		ll.append(n);	ll.printLL();
-		n = new Node(6);
-		ll.append(n);	ll.printLL();
-		n = new Node(4);
-		ll.append(n);	ll.printLL();
-		System.out.println("\nRemoving 5: " + ll.remove(new Node(5)));
-		ll.printLL();
-		System.out.println("\nRemoving 4: " + ll.remove(new Node(4)));
-		ll.printLL();
-		System.out.println("\nRemoving 9: " + ll.remove(new Node(9)));
-		ll.printLL();
-		System.out.println("\nRemoving 11: " + ll.remove(new Node(11)));
-		ll.printLL();
-		n = new Node(2);
-		System.out.println("\nInserting 2 before 1st node: ");
-		ll.insert(n, 1);
-		ll.printLL();
-	}
+    public int deleteAt(int place)
+    {
+        if (isEmpty())
+        {
+            System.out.println("Error-linked list is empty");
+            return 0;
+        }
+        Node temp = start;
+        int counter = 0;
+        int delData = 0;
+        place = Math.abs(place); // no negatives
+        /* redundant! why?
+        if( place == 0 )
+        {
+            delData = start.data;
+            start = start.next;
+            size--;
+            return delData;
+        }
+        */
+        while(counter < size)
+        {
+            if(counter == place)
+            {
+                delData = temp.data;
+                temp = temp.next;
+                size--;
+                break;
+            }
+            temp = temp.next;
+            counter++;
+        }
+        return delData;
+    }
+
+    public void printLinkedList()
+    {
+        Node temp = start;
+        System.out.print("start");
+        while (temp != null)
+        {
+            System.out.print(" -> " + temp.data);
+            temp = temp.next;
+        }
+        System.out.println(" -> null\tsize: " + size);
+    }
+
+    private void insertAt(Node n, int place)
+    {
+        if (isEmpty())
+        {
+            start = n;
+            size = 1;
+            return;
+        }
+        Node temp = start;
+        int counter = 1;
+        while (counter < place)
+        {
+            temp = temp.next;
+            counter++;
+        }
+        n.next = temp.next;
+        temp.next = n;
+        size++;
+    }
+
+    public void insertBefore(Node n, int place)
+    {
+        insertAt(n, place - 1);
+    }
+
+    public void insertAfter(Node n, int place)
+    {
+        insertAt(n, place);
+    }
+
+    // insert after a value in the LL
+    public void insert(Node newNode, Node key)
+    {
+        Node temp = start;
+        while(temp.next != null)
+        {
+            if(temp.data == key.data)
+            {
+                break;
+            }
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+    }
 
 }
-
