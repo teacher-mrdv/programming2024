6b65e80e632a0ef24f09bad68844e86598933057
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-08-14 20:19:32 +0800

    Update Animal.java

--- OOP/Zoo/Zoo/src/Animal.java
+++ OOP/Zoo/Zoo/src/Animal.java
@@ -1,7 +1,20 @@
 public class Animal
 {
+    // Attributes/fields/instance variable
     private String name;
     private int birthYear;
     private boolean isDangerous;
-    
+
+    public Animal(String name, int birthYear, boolean isDangerous)
+    {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.isDangerous = isDangerous;
+    }
+
+    public static void main(String[] args)
+    {
+        Animal fido = new Animal("Fido Dido", 2017, false);
+        System.out.println(fido.name);
+    }
 }
