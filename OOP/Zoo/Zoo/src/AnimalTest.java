54d151980512a1a4fb96d02aa8d7afdf942dfca9
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-09-11 11:38:36 +0800

    Update AnimalTest.java

--- OOP/Zoo/Zoo/src/AnimalTest.java
+++ OOP/Zoo/Zoo/src/AnimalTest.java
@@ -5,25 +5,27 @@ public class AnimalTest
 {
     public static void main(String[] args)
     {
-        //Animal dog = new Animal(); // empty constructor
-        Animal dog;
-        //System.out.println(dog); // error because
-        dog = new Animal("Fido Dido", 2017, false);
-        System.out.println(dog.toString());  // compare before and after toString method is added
+        //Animal newAnimal = new Animal(); // empty constructor
+        Animal newAnimal;
+        //System.out.println(newAnimal); // error because
+        newAnimal = new Animal("Fido Dido", 2017, false);
+        System.out.println(newAnimal.toString());  // compare before and after toString method is added
         // no toString() in Animal class: Animal@30f39991 or similar
         // with toString() in the Animal class: Name: Fido Dido, birthYear: 2017, Dangerous: No
-        System.out.println(dog.getName());
-        // print the remaining attributes of DOG
-        System.out.println(dog.getBirthYear());
-        System.out.println(dog.isDangerous());
-        System.out.println(dog.getDangerous());
-        // change DOG's name to "Snoopy" and print its name again
-        dog.setName("Snoopy");
-        System.out.println(dog.getName());
-        System.out.println(dog.getAge());
-        System.out.println(dog.toString());
-        dog.talk();
-        dog.eat();
+        System.out.println(newAnimal.getName());
+        // print the remaining attributes of newAnimal
+        System.out.println(newAnimal.getBirthYear());
+        System.out.println(newAnimal.isDangerous());
+        System.out.println(newAnimal.getDangerous());
+        // change newAnimal's name to "Snoopy" and print its name again
+        newAnimal.setName("Snoopy");
+        System.out.println(newAnimal.getName());
+        System.out.println(newAnimal.getAge());
+        System.out.println(newAnimal.toString());
+        newAnimal.talk();
+        newAnimal.eat();
+
+
         /*Scanner input = new Scanner(System.in);
         int x = input.nextInt(); // nextInt() is a non-static method
         System.out.println( Math.sqrt(x) ); // sqrt() is a static method
