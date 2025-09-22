0c0609200ff951b93c2348c89ab5acc2864ad512
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-09-22 15:01:26 +0800

    Update Fraction.java

--- OOP/Fraction/src/Fraction.java
+++ OOP/Fraction/src/Fraction.java
@@ -11,6 +11,30 @@ public class Fraction {
         System.out.println(numerator + "/" + denominator);
     }
 
+    // HL recursive version
+    private static int GCD(int a, int b) {
+        if (a == 0)
+            return b;
+        return GCD(b % a, a);
+    }
+
+    // calculate the GCD to simplify a fraction
+    // SL version (iterative -- while loop)
+    private static int gcd(int n, int m) {
+        int gcd;
+        if ( n == m )
+            gcd = n;
+        else {
+            while (n != m) {
+                if ( n < m )
+                    m = m - n;
+                else
+                    n = n - m;
+            }
+        }
+        return n;
+    }
+
     // this replaces the print method
     public String toString() {
         return numerator + "/" + denominator;
