a120fe0f2dc8b5c86075419bcefa8628bff6abca
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2026-08-27 14:43:56 +0800

    updates

--- HL/Builtins/JavaQueueDemoAnswer.java
+++ HL/Builtins/JavaQueueDemoAnswer.java
@@ -3,28 +3,33 @@ import java.util.Queue;
 
 // Hint: read https://www.digitalocean.com/community/tutorials/java-queue
 
-public class JavaQueueDemo
+public class JavaQueueDemoAnswer
 {
     public static void main(String[] args)
     {
         System.out.println("Java QUEUE\n");
         Queue<String> q = new LinkedList<>();
         System.out.println("enqueueing Ed, Anne, Joel, Quesadilla, Darla, Jose:");
+
         // enqueue the strings: Ed, Anne, Joel, Quesadilla, Darla, Jose
-		
-		// output the size of the queue q (6)
-        System.out.println(q + " size(): " );
-
-        // output the head of the queue, without dequeuing it
-        System.out.println("head of the queue: " );
-        System.out.println("Queue status: " + q);
+        q.add("Ed"); q.add("Anne"); q.add("Joel"); q.add("Quesadilla");
+        q.add("Darla"); q.add("Jose");
         
-        // dequeue the head of the queue q and output it (Ed)
-        System.out.println("dequeue: " );
+		// output the size of the queue q (6)
+        System.out.println(q + " size(): " + q.size());
+        
+        // output the head of the queue, without dequeuing it
+        System.out.println("head of the queue: " + q.peek());
         System.out.println("Queue status: " + q);
-
+        // dequeue the head of the queue q and output it (Ed)
+        
+        System.out.println("dequeue: " + q.remove());
+        System.out.println("Queue status: " + q);
         // use a loop to dequeue and print each string of the queue q
-
+		while(!q.isEmpty()) {
+			System.out.println("Dequeue: " + q.remove());
+			System.out.println("Queue status: " + q);
+        }
 		
     }
 }
--- HL/Builtins/JavaStackDemo.java
+++ HL/Builtins/JavaStackDemo.java
@@ -11,19 +11,20 @@ public class JavaStackDemo
         namesStack.push("Ian");
         namesStack.push("Bella");
         System.out.println("Java STACK\n");
-        System.out.print(namesStack);
+        System.out.print("Stack status: " + namesStack);
         System.out.println(" size = " + namesStack.size());
         System.out.println("peek(): " + namesStack.peek()); // shows the top
         // w/o popping it
-        System.out.println("Removing(1): " + namesStack.remove(1));
-        System.out.println(namesStack);
-        System.out.println("Removing(\"Ian\"): " + namesStack.remove("Ian"));
-        System.out.println(namesStack);
+        System.out.println("Popping: " + namesStack.pop());
+        System.out.println("Stack status: " + namesStack);
+        System.out.println("Popping (again): " + namesStack.pop());
+        System.out.println("Stack status: " + namesStack);
 
         while(!namesStack.isEmpty())
         {
             System.out.println("popping: " + namesStack.pop());
         }
+        System.out.println("Stack status: " + namesStack);
 
     }
 
